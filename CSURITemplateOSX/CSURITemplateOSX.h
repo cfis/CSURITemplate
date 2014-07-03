40b9d592f4f57d1ba7acee33094960ece316470a
Charlie Savage <cfis@zerista.com>
2014-07-03 01:21:29 -0600

    OSX Build support.

---
+++ CSURITemplateOSX/CSURITemplateOSX.h
@@ -0,0 +1,13 @@
+//
+//  CSURITemplateOSX.h
+//  CSURITemplateOSX
+//
+//  Created by Charlie Savage on 7/3/14.
+//  Copyright (c) 2014 Cogenta Systems Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CSURITemplateOSX : NSObject
+
+@end
---
+++ CSURITemplateOSX/CSURITemplateOSX.m
@@ -0,0 +1,13 @@
+//
+//  CSURITemplateOSX.m
+//  CSURITemplateOSX
+//
+//  Created by Charlie Savage on 7/3/14.
+//  Copyright (c) 2014 Cogenta Systems Ltd. All rights reserved.
+//
+
+#import "CSURITemplateOSX.h"
+
+@implementation CSURITemplateOSX
+
+@end
